547a06a81f091950fb084eda1c232341a93bc515
akulkudari <akudari@ucsd.edu>
2025-03-12 21:18:48 -0700

    Update

--- app/main.py
+++ app/main.py
@@ -16,6 +16,7 @@ from contextlib import asynccontextmanager
 import app.database.connection as db
 
 
+templates = Jinja2Templates(directory="app")
 class TaskBase(BaseModel):
     title: str
     description: Optional[str] = None
@@ -48,6 +49,50 @@ app = FastAPI(
 @app.get("/", response_class=HTMLResponse)
 async def read_index():
     return FileResponse("app/index.html")
+
+@app.get("/login", response_class=HTMLResponse)
+async def read_login():
+    return FileResponse("app/login.html")
+@app.post("/login")
+async def userlogin(request: Request, email: str = Form(...), password: str = Form(...)):
+    from app.database import create_session, get_user_by_id
+    """Login endpoint to authenticate the user."""
+
+    conn = db.get_db_connection()
+    if conn is None:
+        raise HTTPException(status_code=500, detail="Database connection error")
+    try:
+        cursor = conn.cursor()
+        # Query to find the user by email
+        cursor.execute("SELECT id, email, password_hash FROM users WHERE email = %s", (email,))
+        user = cursor.fetchone()
+        # Check if the user exists
+        if user is None:
+            raise HTTPException(status_code=400, detail="Invalid email or password")
+
+        # Extract the user data (id, email, password_hash)
+        user_id, user_email, stored_password_hash = user
+
+
+        # Verify the password
+        pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
+        if not pwd_context.verify(password, stored_password_hash):
+            return templates.TemplateResponse("login.html", {"request": request, "error": "Invalid email or password"})
+        session_id = str(uuid.uuid4())
+        session = await create_session(user_id, session_id)
+        if not session:
+            response = RedirectResponse(url=f"/login", status_code=302)
+            return response
+        response = RedirectResponse(url=f"/wardrobe", status_code=302)
+        response.set_cookie(key="sessionId", value=session_id, httponly=True, max_age=3600)
+        return response
+
+    except Error as e:
+        raise HTTPException(status_code=500, detail=f"Error during database operation: {e}")
+
+    finally:
+        cursor.close()
+        conn.close()
 
 @app.post("/tasks/", response_model=Task)
 async def create_task(task: TaskCreate):
